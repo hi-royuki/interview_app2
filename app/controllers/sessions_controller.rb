bfd948162a7e7935eeddb92873274803f4222e6f
EC2 Default User <ec2-user@ip-172-31-29-217.ap-northeast-1.compute.internal>
2022-04-12 13:25:24 +0000

    [Fix]ユーザーログイン機能かんせい（仮）

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,25 @@
 class SessionsController < ApplicationController
+before_action :already_login?, except: :destroy
   def new
+  end
+
+  def create
+    # find_byでユーザを見つけてくる
+    user = User.find_by_email(params[:email])
+    # 上の記述でメールで探してきたユーザーのパスワードがあっていれば
+    if user && user.authenticate(params[:password])
+
+      session[:user_id] = user.id
+      redirect_to user_path, notice: "ログインしました"
+    else
+      flash.now[:alert] = "メールアドレスかパスワードが違います。"
+      render :new
+    end
+
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path, notice: "ログアウトしました"
   end
 end

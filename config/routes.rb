dfb6336448e4242755bda47012f00c6633045202
EC2 Default User <ec2-user@ip-172-31-29-217.ap-northeast-1.compute.internal>
2022-04-12 13:28:10 +0000

    [Fix]

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # topページ
+  root 'homes#top'
+
+  # 新規登録
+  resource :user, only:[:new, :create, :show]
+
+  # ログイン
+  get 'login', to: "sessions#new"
+  post 'login', to: "sessions#create"
+  delete 'logout', to: "sessions#destroy"
+
+
 end

e12063efd553118e0422c109d3ff6df2fe428222
EC2 Default User <ec2-user@ip-172-31-29-217.ap-northeast-1.compute.internal>
2022-04-12 13:27:10 +0000

    [Fix] バリデーション設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,26 @@
 class ApplicationController < ActionController::Base
+  # viewでも使えるようにする
+  helper_method :current_user
+
+  def login?
+    if current_user.nil?
+      redirect_to login_path, alert: "ログインする必要があります。"
+    end
+  end
+
+  def already_login?
+    unless current_user.nil?
+      redirect_to user_path, notice: "ログイン済みです。"
+    end
+  end
+
+  def current_user
+    # もしもsession[user_id]がnilではなかったら
+    if session[:user_id]
+      # sessionのidでユーザを見つけてきて、current_userていう変数に入れる
+      # よってcurrent_userというメソッドを実行するとこのcurrent_userが替えってくる
+      current_user ||= User.find(session[:user_id])
+    end
+  end
 end
+
